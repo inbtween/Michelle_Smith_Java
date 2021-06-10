4bb718bed68056b3a84b568cef87e6a04aabec59
Michelle Smith <smith.michelle.a@gmail.com>
2021-06-10 19:59:05 -0400

    refactore quotesWordsAreMagic

--- M2-Challenge-Smith-Michelle/quoteswordsaremagic/src/main/java/com/company/quoteswordsaremagic/SpringFoxConfig.java
+++ M2-Challenge-Smith-Michelle/quoteswordsaremagic/src/main/java/com/company/quoteswordsaremagic/SpringFoxConfig.java
@@ -1,5 +1,5 @@
 package com.company.quoteswordsaremagic;
-
+// .apis ...basePackage("com.company.quotesWordsAreMagic")) should match
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +8,18 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+//go to your local host
+//http://localhost:8080/v3/api-docs
+//View page source
+//copy info and paste in https://editor.swagger.io/ and save yaml file and save in the root folder of the java project
+
 @Configuration
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.company.quoteswordsaremagic"))
+                .apis(RequestHandlerSelectors.basePackage("com.company.quotesWordsAreMagic"))
                 .paths(PathSelectors.any())
                 .build();
     }
